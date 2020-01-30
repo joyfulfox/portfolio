4d0a445f2f6e260245238c8a35570eef9e12501f
joyfulfox <evamarkina@gmail.com>
2020-01-31 01:41:21 +0200

    Updates

---
+++ precache-manifest.42c8782f3f08b29244086a5ecd7b2072.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629b4864285429d946be9a90cb8f042f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "04e795922c87eb24ec27",
+    "url": "/portfolio/static/css/main.e79437ba.chunk.css"
+  },
+  {
+    "revision": "5ad148f96859557c679d",
+    "url": "/portfolio/static/js/2.30e0ed71.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.30e0ed71.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04e795922c87eb24ec27",
+    "url": "/portfolio/static/js/main.80baa0df.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "7ac3eb6464ebe729b35de7975880a33f",
+    "url": "/portfolio/static/media/certificate.7ac3eb64.jpg"
+  },
+  {
+    "revision": "08d82b34bbb227db9df92315e31b88ed",
+    "url": "/portfolio/static/media/landing2.08d82b34.png"
+  },
+  {
+    "revision": "e356c7860156e96e03a755f39fc7f5ce",
+    "url": "/portfolio/static/media/prophile-photo.e356c786.jpg"
+  }
+]);
